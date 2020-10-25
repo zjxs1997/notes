de412708fc6bbd159cf054fa5f1270f1e0ba6b7c
xusheng <1500012780@pku.edu.cn>
2020-10-25 18:22:10 +0800

    update

--- restart_clash_for_windows.ps1
+++ restart_clash_for_windows.ps1
@@ -1,7 +1,9 @@
 # clash for windows会稳定地每隔一段时间崩溃一下。整个重启脚本。
-# 有没有那种可以设置每隔一段时间执行这个脚本的工具？
+# 在task scheduler里可以设置定时任务
+# windows默认不支持跑ps脚本，
 # 执行方式：
 # powershell.exe -executionpolicy bypass -File .\restart_clash_for_windows.ps1
+# 也可以修改一下设置，用管理员权限运行Set-ExecutionPolicy Unrestricted。之后即可直接跑ps脚本了。
 
 taskkill.exe /F /IM 'Clash for Windows.exe'
 echo "clash exited"
