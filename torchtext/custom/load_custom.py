4c7d9614251b90262d6484d2696ffed3eead5ebd
zjxs1997 <1500012780@pku.edu.cn>
2020-11-10 14:32:33 +0800

    update

--- torchtext/custom/load_custom.py
+++ torchtext/custom/load_custom.py
@@ -1,8 +1,8 @@
 # 这个任务的输入和输出都是string的list，从pickle文件中读取数据。
 # 默认两端的batch size都是1了，也正是利用了这一点才能魔改继承原来的field并override加了两行代码。
 # Iterator中返回的对象是个二维tensor，形状为
-# (src_list_len, src_seq_len) 和 target端的，因为是tensor所以肯定有padding，可以考虑写个解padding的小函数
-
+# (src_list_len, src_seq_len) 和 target端的，
+# 因为是tensor所以肯定有padding，写了个解padding的小函数
 
 import pickle
 
@@ -29,9 +29,10 @@ class MyField(Field):
         tensor = self.numericalize(padded, device=device)
         return tensor
 
-
+# 如果要pickle保存的话，要把tokenize改一下，不能用lambda表达式
+# 要解padding的话最好把include_lengths设置成True，
 source_field = MyField(tokenize=lambda x: x.split(), lower=True, batch_first=True)
-target_field = MyField(tokenize=lambda x: x.split(), init_token='<bos>', eos_token='<eos>', lower=True, batch_first=True)
+target_field = MyField(tokenize=lambda x: x.split(), init_token='<bos>', eos_token='<eos>', lower=True, batch_first=True, include_lengths=True)
 
 
 source_list = pickle.load(open("data/source.pkl", 'rb'))
@@ -59,17 +60,38 @@ train_iterator = BucketIterator(train_dataset, batch_size=1, device=device)
 
 for bi, batch in enumerate(train_iterator):
     src = batch.src
-    trg = batch.trg
+    trg, trg_lens = batch.trg
 
     for l in src.tolist():
         for i in l:
             print(source_field.vocab.itos[i], end=' ')
         print()
 
+    print("=" * 20)
+
     for l in trg.tolist():
         for i in l:
             print(target_field.vocab.itos[i], end=' ')
         print()
+    break
 
 # 封装成函数return的话得return两个field（可能只要source的）和iterator
 
+
+# helper functions
+def numericalize(cluster, field):
+    # cluster is list[string]
+    input_data = []
+    for string in cluster:
+        # 注意这个tokenize是没有lower的
+        input_data.append(field.tokenize(string))
+    input_data = [input_data]
+    return field.process(input_data)
+
+def unpad(tensor, tensor_lengths):
+    # tensor is 2d (list len, max seq len)
+    tensor_lengths = tensor_lengths.cpu().tolist()
+    result = []
+    for si, sl in enumerate(tensor_lengths):
+        result.append(tensor[si, :sl])
+    return result
